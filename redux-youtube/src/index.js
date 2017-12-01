0777751cc2739f4bc34e5b383baf2fa8cc1b9ed2
Zoe Zhou <Zoe_Zhou@epam.com>
2017-12-01 18:32:11 +0800

    base framework

---
+++ redux-youtube/src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
+import './index.css';
+
+ReactDOM.render(
+  <Routes history={BrowserRouter} />,
+  document.getElementById('root')
+);
